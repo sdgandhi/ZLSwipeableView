31afdbc9569f51b7400825db3013c69e665cd2b4
Sidhant Gandhi <sidhantg@syntertainment.com>
2015-03-21 09:21:10 -0700

    update scheme

---
+++ ZLSwipeableViewDemo/ZLSwipeableView/ZLSwipeableView.h
@@ -0,0 +1,19 @@
+//
+//  ZLSwipeableView.h
+//  ZLSwipeableView
+//
+//  Created by Sidhant Gandhi on 3/21/15.
+//  Copyright (c) 2015 Zhixuan Lai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZLSwipeableView.
+FOUNDATION_EXPORT double ZLSwipeableViewVersionNumber;
+
+//! Project version string for ZLSwipeableView.
+FOUNDATION_EXPORT const unsigned char ZLSwipeableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLSwipeableView/PublicHeader.h>
+
+
